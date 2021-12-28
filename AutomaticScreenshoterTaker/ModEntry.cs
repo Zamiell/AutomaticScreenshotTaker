9fdb8c8dbc99f201b6383f4a2fa3959824598479
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-12-27 22:49:14 -0500

    initial commit

---
+++ AutomaticScreenshoterTaker/ModEntry.cs
@@ -0,0 +1,46 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace AutomaticScreenshoterTaker
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Player.Warped += this.OnWarped;
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnWarped(object sender, WarpedEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+            {
+                return;
+            }
+
+            var msg = $"Loaded area: {e.NewLocation}.";
+            this.Monitor.Log(msg, LogLevel.Debug);
+        }
+
+        private void TakeScreenshot()
+        {
+            float zoomLevel = 1;
+            string screenshotPath = "a.png";
+
+            string mapScreenshotPath = Game1.game1.takeMapScreenshot(zoomLevel, screenshotPath, () => {});
+        }
+    }
+}
